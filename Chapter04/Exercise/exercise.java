d5c0348cb0d04156e84acd7836c409b7d608c21b
slan0044 <slan0044@student.monash.edu>
2026-06-19 18:43:46 +1000

    Chapter04 test exercise

---
+++ Chapter04/Exercise/exercise.java
@@ -0,0 +1,11 @@
+package Chapter04.Exercise;
+
+import java.util.Scanner;
+
+public class exercise {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int firstNumber = input.nextInt();
+    }
+}
