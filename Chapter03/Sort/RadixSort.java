6f7e466277bdd95aca115007e8e2b98455fd5f0f
slan0044 <slan0044@student.monash.edu>
2026-06-19 17:18:26 +1000

    radix sort

--- Chapter03/Sort/RadixSort.java
+++ Chapter03/Sort/RadixSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class RadixSort {
     public static void main(String[] args){
-        int[] arr = {53, -3, 542, 748, 14, 214};
+        int[] arr = {53, -3, 542, -748, 14, -214};
         radixSortWithNegative(arr);
     }
 
